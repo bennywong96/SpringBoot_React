32519a22447c1873ef136d21107de06ad349d5c8
Benny <benny.wong@academytrainee.com>
2018-07-05 14:30:01 +0100

    Everything apart from edit works

--- demo/src/main/java/modules/DatabaseController.java
+++ demo/src/main/java/modules/DatabaseController.java
@@ -1,9 +1,6 @@
 package modules;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -31,17 +28,16 @@ public class DatabaseController {
         return eRepository.findAll();
     }
 
+    @RequestMapping(value = "/edit/{id}", method = RequestMethod.PUT)
+    public List<Employee> edit(@PathVariable long id, @RequestBody Employee employee){
+        eRepository.save(employee);
+        return eRepository.findAll();
+    }
+
     @DeleteMapping(path= "/delete/{id}")
     public List<Employee> delete(@PathVariable long id){
         eRepository.deleteById(id);
         return eRepository.findAll();
     }
 
-//    @Override
-//    public void run(String... strings) throws Exception {
-//        this.eRepository.save(new Employee("Joe", "Biden", 5));
-//        this.eRepository.save(new Employee("President", "Obama", 8));
-//        this.eRepository.save(new Employee("Crystal",  "Mac", 34));
-//        this.eRepository.save(new Employee("James", "Henry", 33));
-//    }
 }
--- demo/src/main/resources/static/JS/app.js
+++ demo/src/main/resources/static/JS/app.js
@@ -35,15 +35,19 @@ var Navbar = React.createClass({
         ReactDOM.render(<MyInput/>, document.getElementById('root'))
     },
 
+    dashBoard(){
+        ReactDOM.render(<Dash/>, document.getElementById('root'))
+    },
+
     render: function(){
         return( <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="#">AccountApp</a>
+                <a className="navbar-brand" href="#" onClick={this.dashBoard}>AccountApp</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+                    <span className="navbar-toggler-icon"/>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div className="navbar-nav">
-                        <a className="nav-item nav-link active" href="#">DashBoard <span className="sr-only">(current)</span></a>
+                        <a className="nav-item nav-link active" href="#" onClick={this.dashBoard}>DashBoard <span className="sr-only">(current)</span></a>
                         <li className="nav-item dropdown">
                             <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 Accounts
@@ -65,7 +69,7 @@ var NewUsers = React.createClass({
     render: function() {
         return (
             <div>
-            <Navbar />
+                <Navbar />
             </div>
         )
     },
@@ -116,31 +120,49 @@ var MyInput = React.createClass({
     },
     render: function(){
         return(
-            <div className="container">
+            <div>
                 <Navbar />
-                <form onSubmit={this.onSubmit}>
-                <div className="form-group">
-                    <label htmlFor="inputFirstName">First Name:</label>
-                    <input type="text" className="form-control" id="inputFirstName" placeholder="First Name" onChange={this.firstNameChange}/>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="inputSurname">Last Name:</label>
-                    <input type="text" className="form-control" id="inputSurname" placeholder="Last Name" onChange={this.surnameChange}/>
-                </div>
+            <div className="container">
 
-                <div className="form-group">
-                    <label htmlFor="inputAccountNumber"> Account Number:</label>
-                    <input type="text" className="form-control" id="inputAccountNumber" placeholder="Account Number" onChange={this.accountNumberChange}/>
-                </div>
-                <button type="submit" className="btn btn-primary">Add Account</button>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="inputFirstName">First Name:</label>
+                        <input type="text" className="form-control" id="inputFirstName" placeholder="First Name" onChange={this.firstNameChange}/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="inputSurname">Last Name:</label>
+                        <input type="text" className="form-control" id="inputSurname" placeholder="Last Name" onChange={this.surnameChange}/>
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="inputAccountNumber"> Account Number:</label>
+                        <input type="text" className="form-control" id="inputAccountNumber" placeholder="Account Number" onChange={this.accountNumberChange}/>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Add Account</button>
                 </form>
+            </div>
             </div>
         )
     }
 });
 
 
+var Dash = React.createClass({
+    render: function () {
+        return (
+    <div>
+        <Navbar/>
+            <div className="container">
+                <div>
+                    <h1>Welcome</h1>
+                    <p>Please use the navigation bar to go to different sections</p>
+                </div>
+            </div>
+    </div>
+        );
+    }
 
+});
 
 
 //    var UsersForms = React.createClass({
@@ -168,9 +190,11 @@ var Employee = React.createClass({
             }
         });
     },
-    // edit(){
-    //     <CreateDialog />
-    // },
+    edit(){
+        console.log("Nawid sucks");
+
+        ReactDOM.render(<CreateDialog/>, document.getElementById('root'))
+    },
     render: function() {
         if (this.state.display==false) return null;
         else return (
@@ -206,6 +230,7 @@ var EmployeeTable = React.createClass({
                             <th>First Name</th>
                             <th>Surname</th>
                             <th>Account Number</th>
+                            <th>Edit</th>
                             <th>Delete</th>
                         </tr>
                         </thead>
@@ -220,112 +245,116 @@ var EmployeeTable = React.createClass({
 
 
 
-// var CreateDialog = React.createClass ({
-//
-//     constructor(props) {
-//         // noinspection JSAnnotator
-//         super(props);
-//         this.handleSubmit = this.handleSubmit.bind(this);
-//     },
-//
-//     firstNameChange: function(e) {
-//         this.setState({ firstName: e.target.value})
-//     },
-//     surnameChange: function(e) {
-//         this.setState({surname: e.target.value})
-//     },
-//     accountNumberChange: function(e) {
-//         this.setState({accountNumber: e.target.value})
-//     },
-//
-//     onSubmit: function(e){
-//         e.preventDefault();
-//
-//         const data = {
-//             "firstName": this.state.firstName,
-//             "surname": this.state.surname,
-//             "accountNumber": this.state.accountNumber
-//         };
-//         e.target.reset();
-//         const jsonData = JSON.stringify(data);
-//
-//         var settings = {
-//             "async": true,
-//             "crossDomain": true,
-//             "url": "http://localhost:8080/api/employees/add",
-//             "method": "POST",
-//             "headers": {
-//                 "content-type": "application/json",
-//                 "cache-control": "no-cache",
-//                 "postman-token": "3e28dcb4-ba6a-45bd-1807-4c74b1780b6f"
-//             },
-//             "processData": false,
-//             "data": jsonData
-//         }
-//
-//         $.ajax(settings).done(function (response) {
-//             console.log(response);
-//         });
-//     },
-//
-//     handleSubmit(e) {
-//         e.preventDefault();
-//         var newEmployee = {};
-//         this.props.attributes.forEach(attribute => {
-//             newEmployee[attribute] = ReactDOM.findDOMNode(this.refs[attribute]).value.trim();
-//         });
-//         this.props.onCreate(newEmployee);
-//
-//         // clear out the dialog's inputs
-//         this.props.attributes.forEach(attribute => {
-//             ReactDOM.findDOMNode(this.refs[attribute]).value = '';
-//         });
-//
-//         // Navigate away from the dialog to hide it.
-//         window.location = "#";
-//     },
-//
-//     render: function() {
-//         var inputs = this.props.attributes.map(attribute =>
-//             <p key={attribute}>
-//                 <input type="text" placeholder={attribute} ref={attribute} className="field" />
-//             </p>
-//         );
-//
-//         return (
-//             <div>
-//                 <a href="#createEmployee">Create</a>
-//
-//                 <div id="createEmployee" className="modalDialog">
-//                     <div>
-//                         <a href="#" title="Close" className="close">X</a>
-//
-//                         <h2>Create new employee</h2>
-//
-//                         <form onSubmit={this.onSubmit}>
-//                             <div className="form-group">
-//                                 <label htmlFor="inputFirstName">First Name:</label>
-//                                 <input type="text" className="form-control" id="inputFirstName" placeholder="First Name" onChange={this.firstNameChange}></input>
-//                             </div>
-//                             <div className="form-group">
-//                                 <label htmlFor="inputSurname">Last Name:</label>
-//                                 <input type="text" className="form-control" id="inputSurname" placeholder="Last Name" onChange={this.surnameChange()}></input>
-//                             </div>
-//
-//                             <div className="form-group">
-//                                 <label htmlFor="inputAccountNumber"> Account Number:</label>
-//                                 <input type="text" className="form-control" id="inputAccountNumber" placeholder="Account Number" onChange={this.accountNumberChange()}></input>
-//                             </div>
-//                             <button type="submit" className="btn btn-primary">Add Account</button>
-//                         </form>
-//                     </div>
-//                 </div>
-//             </div>
-//         )
-//     }
-//
-// });
+var CreateDialog = React.createClass ({
+
+    // constructor(props) {
+    //     // noinspection JSAnnotator
+    //     super(props);
+    //     this.handleSubmit = this.handleSubmit.bind(this);
+    // },
+    getInitialState: function() {
+        return {typed: ''};
+    },
+    firstNameEdit: function(e) {
+        this.setState({ firstName: e.target.value})
+    },
+    surnameEdit: function(e) {
+        this.setState({surname: e.target.value})
+    },
+    accountNumberEdit: function(e) {
+        this.setState({accountNumber: e.target.value})
+    },
+
+    onSubmit: function(e){
+        e.preventDefault();
+
+        const data = {
+            //"id": this.state.id,
+            "firstName": this.state.firstName,
+            "surname": this.state.surname,
+            "accountNumber": this.state.accountNumber
+        };
+        e.target.reset();
+        const jsonData = JSON.stringify(data);
+
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "http://localhost:8080/api/employees/edit/"+ this.props.employee.id,
+            "method": "PUT",
+            "headers": {
+                "content-type": "application/json",
+                "cache-control": "no-cache",
+                "postman-token": "7e3a162c-b6b8-fefe-1056-7f7241d4e856"
+            },
+            "processData": false,
+            "data": jsonData
+        }
+
+        $.ajax(settings).done(function (response) {
+            console.log(response);
+        });
+        window.location = "#";
+        console.log(this.props.employee.id);
+
+        ReactDOM.render(<App />, document.getElementById('root'))
+    },
+    //
+    // handleSubmit(e) {
+    //     e.preventDefault();
+    //     var newEmployee = {};
+    //     this.props.attributes.forEach(attribute => {
+    //         newEmployee[attribute] = ReactDOM.findDOMNode(this.refs[attribute]).value.trim();
+    //     });
+    //     this.props.onCreate(newEmployee);
+    //
+    //     // clear out the dialog's inputs
+    //     this.props.attributes.forEach(attribute => {
+    //         ReactDOM.findDOMNode(this.refs[attribute]).value = '';
+    //     });
+    //
+    //     // Navigate away from the dialog to hide it.
+    //
+    // },
+
+    render: function() {
+        // var inputs = this.props.attributes.map(attribute =>
+        //     <p key={attribute}>
+        //         <input type="text" placeholder={attribute} ref={attribute} className="field" />
+        //     </p>
+        // );
+
+        return (
+            <div>
+                <div id="editEmployee" className="modalDialog">
+                    <div>
+                        <a href="#" title="Close" className="close">X</a>
+                        <h2>Edit Employee</h2>
+
+                        <form onSubmit={this.onSubmit}>
+                            <div className="form-group">
+                                <label htmlFor="firstNameEdit">First Name:</label>
+                                <input type="text" className="form-control" id="firstNameEdit" placeholder="First Name" onChange={this.firstNameEdit}/>
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="surnameEdit">Last Name:</label>
+                                <input type="text" className="form-control" id="surnameEdit" placeholder="Last Name" onChange={this.surnameEdit}/>
+                            </div>
+
+                            <div className="form-group">
+                                <label htmlFor="accountNumberEdit"> Account Number:</label>
+                                <input type="text" className="form-control" id="accountNumberEdit" placeholder="Account Number" onChange={this.accountNumberEdit}/>
+                            </div>
+                            <button type="submit" className="btn btn-primary">Save</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+});
 
 
 
-ReactDOM.render( <NewUsers />, document.getElementById('root') );
+ReactDOM.render( <Dash />, document.getElementById('root') );
